61381f765f0d013fd9a249b070496c029b2d4005
vagrant <gio_wan@live.ca>
2021-02-12 21:52:00 +0000

    added a bitefish class of fish

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,10 @@
+class BiteFish extends Fish {
+
+  // overriding constructor
+  constructor(options) {
+    super(options); // Call super to run the code inside 'Fish''s constructor
+    this.imageUri = '/images/bite_fish.gif'; // Set the image
+  }
+
+
+}
